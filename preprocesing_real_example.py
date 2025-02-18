9b3a6902709bc8be9979bc39a8d6c721e4b1d61f
DanialNaseri_2000 <Danialnaseri1379@gmail.com>
2025-02-18 22:11:49 +0330

    Issue date columns into numbers

--- preprocesing_real_example.py
+++ preprocesing_real_example.py
@@ -2,13 +2,13 @@ import numpy as np
 np.set_printoptions(suppress= True, linewidth=100,precision=2)
 
 #Importing the data
-raw_data_np = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\loan-data.csv",
+raw_data_np = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\A-Real-Example-of-Loan-Data-Preprocessing-with-Numpy\loan-data.csv",
                             delimiter=';',
                             skip_header=1,
                             autostrip=True)
 
 #checking for incomplete data
-print(np.isnan(raw_data_np).sum())
+#print(np.isnan(raw_data_np).sum())
 
 temporary_fill = np.nanmax(raw_data_np) + 1
 temporary_mean = np.nanmean(raw_data_np, axis=0)
@@ -22,14 +22,14 @@ columns_string = np.argwhere(np.isnan(temporary_mean)).squeeze()
 columns_numeric = np.argwhere(np.isnan(temporary_mean)  == False).squeeze()
 
 #Ri-importing the dataset
-loan_data_string = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\loan-data.csv",
+loan_data_string = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\A-Real-Example-of-Loan-Data-Preprocessing-with-Numpy\loan-data.csv",
                                  delimiter=';',
                                  skip_header=1,
                                  autostrip = True,
                                  usecols=columns_string,
                                  dtype = str)
 
-loan_data_numeric = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\loan-data.csv",
+loan_data_numeric = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\A-Real-Example-of-Loan-Data-Preprocessing-with-Numpy\loan-data.csv",
                                  delimiter=';',
                                  skip_header=1,
                                  autostrip = True,
@@ -37,7 +37,7 @@ loan_data_numeric = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for nump
                                  filling_values= temporary_fill)
 
 #The name of the columns
-header_full = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\loan-data.csv",
+header_full = np.genfromtxt(r"C:\Users\Danie\Desktop\real example for numpy\A-Real-Example-of-Loan-Data-Preprocessing-with-Numpy\loan-data.csv",
                             delimiter=';',
                             skip_footer=raw_data_np.shape[0],
                             autostrip=True,
@@ -59,8 +59,12 @@ checkpoint_test = checkpoint("checkpoint-test",
 #manipulating string columns
 header_string[0] = "issue_date"
 loan_data_string[:,0] = np.char.strip(loan_data_string[:,0],"-15")
-
-
+months = np.array(['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov',
+                   'Dec'])
+for i in range(13):
+    loan_data_string[:,0] = np.where(loan_data_string[:,0] == months[i],
+                                     i,
+                                     loan_data_string[:,0])
 
 
 
